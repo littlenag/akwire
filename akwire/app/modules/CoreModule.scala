78b0eee94618a16e0e0b2415e0a14978bd5976d7
Mark Kegel <mark.kegel@gmail.com>
2014-09-03 21:38:43 -0500

    forgot a file

---
+++ akwire/app/modules/CoreModule.scala
@@ -0,0 +1,35 @@
+package modules
+
+import akka.actor.ActorSystem
+import controllers.Application
+import org.slf4j.{LoggerFactory, Logger}
+import play.api.Configuration
+import scaldi.Module
+import services.{CoreServices, AlertingEngine, SimpleUUIDGenerator, UUIDGenerator}
+
+class CoreModule extends Module {
+  private final val logger: Logger = LoggerFactory.getLogger(classOf[CoreModule])
+
+  logger.debug("Binding dependencies")
+
+  // binding to "foo" <= IS EQUIVALENT TO => bind[String] to "foo"
+
+  import conf.AkkaContext._
+  import com.typesafe.config._
+
+  binding to new controllers.Application
+  binding to new controllers.Agents
+  binding to new controllers.Detectors
+  binding to new controllers.Ingest
+  binding to new controllers.Roles
+  binding to new controllers.Teams
+  binding to new controllers.Users
+
+  binding to new AlertingEngine
+  binding to new CoreServices
+
+
+  bind[ActorSystem] to ActorSystem("AkkaScalaSpring")
+  bind[Configuration] to new play.api.Configuration(ConfigFactory.load("conf/application.conf"))
+  bind[UUIDGenerator] to new SimpleUUIDGenerator
+}
