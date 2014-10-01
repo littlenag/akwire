500bf8ab7471a5edd80542edb5dfdbd9912cbfe0
Mark Kegel <mark.kegel@gmail.com>
2014-10-01 01:18:44 -0500

    comments

--- akwire/test/services/AlertingEngineTest.scala
+++ akwire/test/services/AlertingEngineTest.scala
@@ -35,32 +35,29 @@ class AlertingEngineTest extends Specification with Mockito {
       //val rule = new Rule("r1", """(where (host "h1") trigger)""")
       //val rule = new Rule("r1", """(where true trigger)""")
       //val rule = new Rule("r1", "prn")
-      //val rule = new Rule("r1", "akwire.streams/trigger")
       // where not foo resolve
-
-      val team = new Team(tid, "t1", List(rule1), new DateTime(), true)
-
-      engine.loadAlertingRule(team, rule1)
 
       // v = f(t), v_now = f(t_now)
 
+      val team = new Team(tid, "t1", List(rule1, rule2), new DateTime(), true)
+
+      // first rule
+
+      engine.loadAlertingRule(team, rule1)
+
       val obs = new ObservedMeasurement("i1", "h1", "t1", "k1", 5)
 
-      obs.host mustEqual "h1"
-
       engine.inspect(obs)
-
-      //there was one(persistence).persistAlert _
 
       // next rule
 
       engine.loadAlertingRule(team, rule2)
 
-      // v = f(t), v_now = f(t_now)
-
       val obs2 = new ObservedMeasurement("i1", "h1", "t1", "k1", 15)
 
       engine.inspect(obs2)
+
+      // test alerts
 
       there was two(persistence).persistAlert _
     }
