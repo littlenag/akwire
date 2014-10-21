75aea58b58babfbf716e2f5160b280079aed4c46
Mark Kegel <mark.kegel@gmail.com>
2014-10-21 10:02:59 -0500

    new file

---
+++ akwire/app/controllers/Auth.scala
@@ -0,0 +1,14 @@
+package controllers
+
+import models.User
+import securesocial.controllers.BaseLoginPage
+import play.api.mvc.{ AnyContent, Action }
+import play.api.Logger
+import securesocial.core.{ RuntimeEnvironment }
+
+class Auth(implicit override val env: RuntimeEnvironment[User]) extends BaseLoginPage[User] {
+  override def login: Action[AnyContent] = {
+    Logger.debug("using Auth Controller")
+    super.login
+  }
+}
